9da4f7200b7ebca5a2ff99345542109290a5f91e
zur-quin <89942742+zur-quin@users.noreply.github.com>
2023-10-14 17:22:06 -0400

    SVD plots of singular values for 7a

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
+use std::fs;
+use std::fs::File;
+use std::io::Write;
 use image::DynamicImage::ImageLuma8;
 use image::GrayImage;
 use nalgebra::DMatrix;
+use nalgebra::SVD;
+use show_image::WindowOptions;
 use show_image::create_window;
 use show_image::event;
 
@@ -15,17 +20,29 @@ fn main() {
     let A = DMatrix::from_row_iterator(
         img.height() as usize,
         img.width() as usize,
-        img.as_raw().iter().copied(),
+        img.as_raw().iter().map(|x| { return *x as f64}),
     );
 
-    println!("{A}");
+    // println!("{A}");
 
     // check matrix is still the same image
     let im2 = GrayImage::from_fn(A.ncols() as u32, A.nrows() as u32, |c, r| {
-        return image::Luma([A[(r as usize, c as usize)]]);
+        return image::Luma([A[(r as usize, c as usize)] as u8]);
     });
     let window2 = create_window("Image from Matrix", Default::default()).unwrap();
     window2.set_image("Figure 1.5", im2).unwrap();
+
+    let svd=SVD::new(A,true, true);
+    fs::create_dir_all("./out").unwrap();
+    let mut out = File::create("./out/all_singular_values7.dat").unwrap();
+    writeln!(out, "#all singular values of A").unwrap();
+    for s in &svd.singular_values {
+        writeln!(out, "{}", s/svd.singular_values[0]).unwrap();
+    }
+    // svd.singular_values;
+    println!("finished printing");
+
+
 
     // keep images up until closed
     for event in window.event_channel().unwrap() {}
